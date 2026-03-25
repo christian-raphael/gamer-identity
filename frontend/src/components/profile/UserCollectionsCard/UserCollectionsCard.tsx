d91c3f6994683ff67ee5b4ce8e35ae2164bce91c
Christian Raphael <christianraphael.1309@gmail.com>
2026-03-25 20:54:43 -0300

    add edit collections button

--- frontend/src/components/profile/UserCollectionsCard/UserCollectionsCard.tsx
+++ frontend/src/components/profile/UserCollectionsCard/UserCollectionsCard.tsx
@@ -1,6 +1,7 @@
 import UserCollectionItem from "../UserCollectionItem/UserCollectionItem";
 import "./UserCollectionsCard.css";
 import { userCollections } from "../../../assets/games";
+import EditIcon from '@mui/icons-material/Edit';
 
 function buildCollectionsItemsElements() {
     return userCollections.slice(0, 3).map((collection) => (
@@ -16,6 +17,12 @@ function UserCollectionsCard() {
       <h2>Your Collections</h2>
       <div className="main-collections-area">
         {collectionsItemsElements}
+      </div>
+      <div className="edit-collections-container">
+        <button> 
+          <EditIcon fontSize="small"/> 
+          Edit Collections
+        </button>
       </div>
     </div>
   );
